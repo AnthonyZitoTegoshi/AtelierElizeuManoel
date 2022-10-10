e7df2a07a8149849ddd0a2d012b8b7c60888bce3
Anthony Zito Tegoshi <tegoshianthonyzito@gmail.com>
2022-10-09 22:13:47 -0300

    Molde de conexão PHP com BD

--- etc/conf.php
+++ etc/conf.php
@@ -1,10 +1,35 @@
 <?php
 
-$ROOT_PATH = "";
+// Inicia ou resume uma sessão
+session_start();
+
+// Verifica qual é o local em que o código está sendo executado
 if ($_SERVER["HTTP_HOST"] == "localhost") {
-    $ROOT_PATH = "http://localhost/AtelierElizeuManoel";
+    // Seta o caminho root
+    $_SESSION["ROOT_PATH"] = "http://localhost/AtelierElizeuManoel";
+    // Seta o host para conexão com BD
+    $_SESSION["CONNECTION"]["HOSTNAME"] = "localhost"; // !change
+    // Seta o usuário para conexão com BD
+    $_SESSION["CONNECTION"]["USERNAME"] = "anthony";
+    // Seta a senha para conexão com BD
+    $_SESSION["CONNECTION"]["PASSWORD"] = "123456";
+    // Seta o banco de dados para conexão
+    $_SESSION["CONNECTION"]["DATABASE"]["NAME"] = "atelier";
 } else {
-    $ROOT_PATH = "https://hostdeprojetosdoifsp.gru.br/atelier";
+    // Seta o caminho root
+    $_SESSION["ROOT_PATH"] = "https://hostdeprojetosdoifsp.gru.br/atelier";
+    // Seta o host para conexão com BD
+    $_SESSION["CONNECTION"]["HOSTNAME"] = "hostdeprojetosdoifsp.gru.br"; // !change
+    // Seta o usuário para conexão com BD
+    $_SESSION["CONNECTION"]["USERNAME"] = "hostdeprojetos_atelier";
+    // Seta a senha para conexão com BD
+    $_SESSION["CONNECTION"]["PASSWORD"] = "wT8p8antps9tumT";
+    // Seta o banco de dados para conexão
+    $_SESSION["CONNECTION"]["DATABASE"]["NAME"] = "hostdeprojetos_atelier";
 }
+
+// Seta a tabela de usuário e seus campos
+$_SESSION["CONNECTION"]["DATABASE"]["TABLES"]["USERS"]["NAME"] = "Users";
+$_SESSION["CONNECTION"]["DATABASE"]["TABLES"]["USERS"]["FIELDS"] = array("id", "name", "email", "password"); // !change
 
 ?>
