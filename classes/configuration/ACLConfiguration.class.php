76c8adfb0212337f78d7d8d5fd9dfb08bb7de61b
ThiagoMRibeiro <77808417+ThiagoMoralesRibeiro@users.noreply.github.com>
2022-11-10 18:14:24 -0300

    acl-new version
    desc

--- classes/configuration/ACLConfiguration.class.php
+++ classes/configuration/ACLConfiguration.class.php
@@ -11,31 +11,119 @@
 
         function checkPermission($permission, $id, $groupId){
             if (!$this->userPermissions($id, $permission)) {
-                echo 'false';
+                return false;
             }
 
             if (!$this->groupPermissions($groupId, $permission)) {
-                echo 'false';
+               return false;
             }
 
-            echo 'true';
+            return true;
         }
 
 
-        function userPermissions($permission, $userId){
-            $this->database->query("SELECT COUNT(*) AS count FROM UserPermissions WHERE permissionName='$permission' AND id='$userId' ");
-        }
+        
 
         public function groupPermissions($permission, $groupId){
             $this->database->query("SELECT COUNT(*) AS count FROM GroupPermissions WHERE permissionName='$permission' AND groupId='$groupId' ");
 
-            $fetch = $this->db->fetch_assoc();
+            $fetch = $this->database->fetch_assoc();
 
             if ($fetch['count'] > 0) {
                 $this->database->query("SELECT * FROM GroupPermissions WHERE permissionName='$permission' AND groupId='$groupId' ");
             }
 
+            if ($fetch['permission_type']==0) {
+                return false;
+            }
+
+            return true;
+
         }
+
+        public function userPermissions($permission, $id){
+            $this->database->query("SELECT COUNT(*) AS count FROM UserPermissions WHERE permissionName='$permission' AND id='$id' ");
+
+            $fetch = $this->database->fetch_assoc();
+
+            if ($fetch['count'] > 0) {
+                $this->database->query("SELECT * FROM GroupPermissions WHERE permissionName='$permission' AND groupId='$id' ");
+            }
+
+            if ($fetch['permission_type']==0) {
+                return false;
+            }
+
+            return true;
+
+        }<?php
+    
+
+    class ACL{
+        private $database;
+        private $emptyUser = false;
+        
+        function __construct(){
+            $this->database = new DatabaseConnection();
+        }
+
+        function checkPermission($permission, $id, $groupId){
+            if (!$this->userPermissions($id, $permission)) {
+                return false;
+            }
+
+            if (!$this->groupPermissions($groupId, $permission)) {
+               return false;
+            }
+
+            return true;
+        }
+
+
+        
+
+        public function groupPermissions($permission, $groupId){
+            $this->database->query("SELECT COUNT(*) AS count FROM GroupPermissions WHERE permissionName='$permission' AND groupId='$groupId' ");
+
+            $fetch = $this->database->fetch_assoc();
+
+            if ($fetch['count'] > 0) {
+                $this->database->query("SELECT * FROM GroupPermissions WHERE permissionName='$permission' AND groupId='$groupId' ");
+            }
+
+            if ($fetch['permission_type']==0) {
+                return false;
+            }
+
+            return true;
+
+        }
+
+        public function userPermissions($permission, $id){
+            $this->database->query("SELECT COUNT(*) AS count FROM UserPermissions WHERE permissionName='$permission' AND id='$id' ");
+
+            $fetch = $this->database->fetch_assoc();
+
+            if ($fetch['count'] > 0) {
+                $this->database->query("SELECT * FROM GroupPermissions WHERE permissionName='$permission' AND groupId='$id' ");
+            }
+
+            if ($fetch['permission_type']==0) {
+                return false;
+            }
+
+            return true;
+
+        }
+
+
+        
+        
+
+    }
+    
+
+?>
 
 
         
