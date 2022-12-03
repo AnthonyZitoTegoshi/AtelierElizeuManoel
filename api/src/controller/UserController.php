e10e825cced5d0fa466a1fa33d411f11e51aabd7
ThiagoMRibeiro05 <thiagomoralesribeiro@gmail.com>
2022-12-03 20:19:46 -0300

    changes

--- api/src/controller/UserController.php
+++ api/src/controller/UserController.php
@@ -1,4 +1,5 @@
 <?php
+    
     namespace App\Controller;
 
     use App\Helper\GenerateHelper as GenerateHelper;
@@ -45,57 +46,53 @@
                 ResponseHelper::send(RESPONSE_SUCCESS, 'O cadastro foi concluído');
             } 
             else {
-                echo 
-                ResponseHelper::send(REQUEST_ERROR, 'Houve um erro na efetivação do cadastro');
-
+                ResponseHelper::send(REQUEST_ERROR, $user->fail()->getMessage());
             }
                         
         }
-
-        public function requestPassword(array $data): void {
-            $email = $data['email'];
-            if (InputHelper::isValidEmail($email)) {
-                if ((new UserModel)->find('email = :email', 'email=' . $email)->count() > 0) {
-                    $token = GenerateHelper::randomToken();
-                    $hashedToken = HashHelper::encrypt($token, substr($token, 0, 12));
-                    $expireDate = new \DateTime();
-                    $expireDate->add(new \DateInterval('P1D'));
+        
+        // public function requestPassword(array $data): void {
+        //     $email = $data['email'];
+        //     if (InputHelper::isValidEmail($email)) {
+        //         if ((new UserModel)->find('email = :email', 'email=' . $email)->count() > 0) {
+        //             $token = GenerateHelper::randomToken();
+        //             $hashedToken = HashHelper::encrypt($token, substr($token, 0, 12));
+        //             $expireDate = new \DateTime();
+        //             $expireDate->add(new \DateInterval('P1D'));
     
-                    $passwordRequest = (new PasswordRequestModel())->find(
-                        'email = :email',
-                        'email=' . $email
-                    );
-                    if ($passwordRequest->count() > 0) {
-                        $passwordRequest = $passwordRequest->fetch();
-                    }
-                    $passwordRequest->email = $email;
-                    $passwordRequest->token = $hashedToken;
-                    $passwordRequest->expire_date = $expireDate->format(DEFAULT_DATETIME_FORMAT);
+        //             $passwordRequest = (new PasswordRequestModel())->find(
+        //                 'email = :email',
+        //                 'email=' . $email
+        //             );
+        //             if ($passwordRequest->count() > 0) {
+        //                 $passwordRequest = $passwordRequest->fetch();
+        //             }
+        //             $passwordRequest->email = $email;
+        //             $passwordRequest->token = $hashedToken;
+        //             $passwordRequest->expire_date = $expireDate->format(DEFAULT_DATETIME_FORMAT);
     
-                    $resetPasswordUrl = Server::getRootUrl() . '/reset_password.html?email=' .
-                        $email . '&token=' . $token;
+        //             $resetPasswordUrl = Server::getRootUrl() . '/reset_password.html?email=' .
+        //                 $email . '&token=' . $token;
     
-                    if ($passwordRequest->save()) {
-                        if (EmailHelper::send($email, 'Recuperação de senha', $resetPasswordUrl)) {
-                            ResponseHelper::send(
-                                RESPONSE_SUCCESS,
-                                'Um email foi enviado com um link para resetar sua senha'
-                            );
-                        } else {
-                            ResponseHelper::send(RESPONSE_ERROR, 'Não foi possível enviar email');
-                        }
-                    } else {
-                        ResponseHelper::send(RESPONSE_ERROR, 'Ocorreu um erro requisitar nova senha');
-                    }
-                } else {
-                    ResponseHelper::send(REQUEST_ERROR, 'O usuário não existe');
-                }
-            } else {
-                ResponseHelper::send(REQUEST_ERROR, 'Email inválido');
-            }
-        }
+        //             if ($passwordRequest->save()) {
+        //                 if (EmailHelper::send($email, 'Recuperação de senha', $resetPasswordUrl)) {
+        //                     ResponseHelper::send(
+        //                         RESPONSE_SUCCESS,
+        //                         'Um email foi enviado com um link para resetar sua senha'
+        //                     );
+        //                 } else {
+        //                     ResponseHelper::send(RESPONSE_ERROR, 'Não foi possível enviar email');
+        //                 }
+        //             } else {
+        //                 ResponseHelper::send(RESPONSE_ERROR, 'Ocorreu um erro requisitar nova senha');
+        //             }
+        //         } else {
+        //             ResponseHelper::send(REQUEST_ERROR, 'O usuário não existe');
+        //         }
+        //     } else {
+        //         ResponseHelper::send(REQUEST_ERROR, 'Email inválido');
+        //     }
+        // }
     }
     
-
-
 ?>
